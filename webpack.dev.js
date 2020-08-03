451da4b548a2e4bc56b492137270bb264aed1cf7
Eason <a0956031116@gmail.com>
2020-08-03 22:31:08 +0800

    add webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,16 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common')
 
 const devConfig = {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    // watchContentBase: true,
+    // contentBase: path.resolve(__dirname, 'static'),
+    inline: true,
+    open: true,
+    // host: '0.0.0.0',
+    // port: 8080
+    // disableHostCheck: false
+  }
 }
 
 module.exports = merge(commonConfig, devConfig)
